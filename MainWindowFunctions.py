c6b6b8f5c8964f2d6b74a8993953b73bbcb7ecd0
Genshirog <imbaxgx1fromyt@gmail.com>
2024-10-10 14:45:45 +0800

    Added New Functions

--- MainWindowFunctions.py
+++ MainWindowFunctions.py
@@ -1,26 +1,49 @@
 class MainWindowFunctions:
   def __init__(self, SELF):
     self.MW = SELF
-
+    self.set_masked = True
+    self.set_masked_reg = True
   def SWITCH_RegisterFrame(self):
     self.MW.GET_RootWindowObject().title('Financial Tracker / Register')
     self.MW.GET_LeftFrameObject('LOGINFRAME').grid_remove()
     self.MW.GET_LeftFrameObject('REGISTERFRAME').grid(row=0, column=0)
+    self.MW.GET_STRINGVARS('LOGIN','USERNAME').set('')
+    self.MW.GET_STRINGVARS('LOGIN','PASSWORD').set('')
 
   def SWITCH_LoginFrame(self):
     self.MW.GET_RootWindowObject().title('Financial Tracker / Login')
     self.MW.GET_LeftFrameObject('REGISTERFRAME').grid_remove()
     self.MW.GET_LeftFrameObject('LOGINFRAME').grid(row=0, column=0)
-
+    self.MW.GET_STRINGVARS('REGISTER','FIRSTNAME').set('')
+    self.MW.GET_STRINGVARS('REGISTER','LASTNAME').set('')
+    self.MW.GET_STRINGVARS('REGISTER','AGE').set('')
+    self.MW.GET_STRINGVARS('REGISTER','ADDRESS').set('')
+    self.MW.GET_STRINGVARS('REGISTER','USERNAME').set('')
+    self.MW.GET_STRINGVARS('REGISTER','PASSWORD').set('')
+  
   def BUTTON_LI_Password(self):
-    if self.MW.Is_Password_Masked('GET', 'LOGIN'):
+    if self.set_masked:
       self.MW.GET_ENTRY_Password_Objects('LOGIN').configure(show='')
       self.MW.GET_BUTTON_Password_Objects('LOGIN').configure(image=self.MW.CTKIMAGE('hide_eye', (40, 20)))
       self.MW.Is_Password_Masked('SET', 'LOGIN', False)
+      self.set_masked = False
     else:
       self.MW.GET_ENTRY_Password_Objects('LOGIN').configure(show='*')
       self.MW.GET_BUTTON_Password_Objects('LOGIN').configure(image=self.MW.CTKIMAGE('show_eye', (40, 20)))
       self.MW.Is_Password_Masked('SET', 'LOGIN', True)
+      self.set_masked = True
+  
+  def BUTTON_LI_Password_Register(self):
+    if self.set_masked_reg:
+      self.MW.GET_ENTRY_Password_Objects('REGISTER').configure(show='')
+      self.MW.GET_BUTTON_Password_Objects('REGISTER').configure(image=self.MW.CTKIMAGE('hide_eye', (40, 20)))
+      self.MW.Is_Password_Masked('SET', 'REGISTER', False)
+      self.set_masked_reg = False
+    else:
+      self.MW.GET_ENTRY_Password_Objects('REGISTER').configure(show='*')
+      self.MW.GET_BUTTON_Password_Objects('REGISTER').configure(image=self.MW.CTKIMAGE('show_eye', (40, 20)))
+      self.MW.Is_Password_Masked('SET', 'REGISTER', True)
+      self.set_masked_reg = True
 
   def BUTTON_Login(self):...
 
