2b8d75933e673486a4fe100f7cacbcbf72b0256c
Genshirog <imbaxgx1fromyt@gmail.com>
2024-10-12 01:03:55 +0800

    Adjusted The table

--- List.py
+++ List.py
@@ -82,7 +82,7 @@ class List:
         return self.__get_mainframe_width(0.8)
     
     def __get_table_list_height(self):
-        return self.__get_mainframe_height(0.6)
+        return self.__get_mainframe_height(0.5)
     
     def _center_frame(self):#It is the method that holds all of the frames
         self.__set_main_frame()
@@ -140,49 +140,81 @@ class List:
     
     def __table_list_frame(self):
         self.__table_list = ctk.CTkFrame(self.__get_main_frame(), width=self.__get_table_list_width(), height= self.__get_table_list_height(), fg_color="#696969")
-        self.__table_list.place(relx = 0.5, rely = 0.6, anchor="center")
-        self.__table()
+        self.__table_list.place(relx = 0.5, rely = 0.5, anchor="center")
+        self.__table_needs()
+        self.__table_wants()
 
     def __get_table_list_frame(self):
         return self.__table_list
     
-    def __table(self):
-        self.__style = ttk.Style()
-        self.__style.configure("Treeview", font=("Poppins", 20), rowheight=85, bordercolor="Black", borderwidth=2,background="black", fieldbackground="black", foreground="white")
-        self.__style.configure("Treeview.Heading", font=("Poppins", 25, "bold"),bordercolor="Black", borderwidth=2, padding=(5,10))
-        self.__table_display = ttk.Treeview(self.__get_table_list_frame(), columns=('col1', 'col2', 'col3', 'col4'),show= 'headings')
-        self.__table_display.heading('col1', text= 'Needs')
-        self.__table_display.heading('col2', text= 'Amount')
-        self.__table_display.heading('col3', text= 'Wants')
-        self.__table_display.heading('col4', text= 'Amount')
-        self.__table_display.column('col1', width= 100, stretch= True,anchor="center")
-        self.__table_display.column('col2', width= 100, stretch= True,anchor="center")
-        self.__table_display.column('col3', width= 100, stretch= True,anchor="center")
-        self.__table_display.column('col4', width= 100, stretch= True,anchor="center")
-        self.__table_display.place(relx=0.5, rely=0.5, relheight=1, relwidth= 1,anchor="center")
-        self.__table_display.insert('','end', values=('', '', '', ''))
-        self.__table_display.insert('','end', values=('', '', '', ''))
-        self.__table_display.bind("<ButtonPress-1>", self.handle_click_category)
+    def __table_needs(self):
+        self.__style_needs = ttk.Style()
+        self.__style_needs.configure("Needs.Treeview", font=("Poppins", 20), rowheight=85, bordercolor="Black", borderwidth=2,background="black", fieldbackground="black", foreground="white")
+        self.__style_needs.configure("Needs.Treeview.Heading", font=("Poppins", 25, "bold"),bordercolor="Black", borderwidth=2, padding=(5,10))
+        self.__table_display_needs = ttk.Treeview(self.__get_table_list_frame(), columns=('col1', 'col2'),show= 'headings', style="Needs.Treeview")
+        self.__table_display_needs.heading('col1', text= 'Needs')
+        self.__table_display_needs.heading('col2', text= 'Amount')
+        self.__table_display_needs.column('col1', width= 100, stretch= True,anchor="center")
+        self.__table_display_needs.column('col2', width= 100, stretch= True,anchor="center")
+        self.__table_display_needs.place(relx=0.25, rely=0.1, relheight=1, relwidth= 0.4,anchor="n")
+        self.__table_display_needs.insert('','end', values=('', '', '', ''))
+        self.__table_display_needs.insert('','end', values=('', '', '', ''))
+        self.__table_display_needs.bind("<ButtonPress-1>", self.handle_click_category_needs)
 
-    def handle_click_category(self, event):
+    def __table_wants(self):
+        self.__style_wants = ttk.Style()
+        self.__style_wants .configure("Wants.Treeview", font=("Poppins", 20), rowheight=85, bordercolor="Black", borderwidth=2,background="black", fieldbackground="black", foreground="white")
+        self.__style_wants .configure("Wants.Treeview.Heading", font=("Poppins", 25, "bold"),bordercolor="Black", borderwidth=2, padding=(5,10))
+        self.__table_display_wants = ttk.Treeview(self.__get_table_list_frame(), columns=('col3', 'col4'),show= 'headings', style="Wants.Treeview")
+        self.__table_display_wants.heading('col3', text= 'Wants')
+        self.__table_display_wants.heading('col4', text= 'Amount')
+        self.__table_display_wants.column('col3', width= 100, stretch= True,anchor="center")
+        self.__table_display_wants.column('col4', width= 100, stretch= True,anchor="center")
+        self.__table_display_wants.place(relx=0.75, rely=0.1, relheight=1, relwidth= 0.4,anchor="n")
+        self.__table_display_wants.insert('','end', values=('', '', '', ''))
+        self.__table_display_wants.insert('','end', values=('', '', '', ''))
+        self.__table_display_wants.bind("<ButtonPress-1>", self.handle_click_category_wants)
+
+    def handle_click_category_needs(self, event):
         """Handle the click event for both preventing drag and clearing selection."""
         # Check if the click is on a separator (to prevent dragging)
-        if self.__table_display.identify_region(event.x, event.y) == "separator":
+        if self.__table_display_needs.identify_region(event.x, event.y) == "separator":
             return "break"  # Prevent dragging if clicked on a separator
 
         # Handle clearing selection on double click
-        selected_item = self.__table_display.selection()
-        current_focus = self.__table_display.focus()
+        selected_item = self.__table_display_needs.selection()
+        current_focus = self.__table_display_needs.focus()
 
         if selected_item:
             if current_focus == selected_item[0]:
                 # Deselect if the same item is clicked again
-                self.__table_display.selection_remove(selected_item[0])
-                self.__table_display.selection_clear()
+                self.__table_display_needs.selection_remove(selected_item[0])
+                self.__table_display_needs.selection_clear()
                 return "break"  # Prevent any further processing
 
         # Update the last clicked item
-        self.last_clicked_item = current_focus  
+        self.last_clicked_item = current_focus
+
+    def handle_click_category_wants(self, event):
+        """Handle the click event for both preventing drag and clearing selection."""
+        # Check if the click is on a separator (to prevent dragging)
+        if self.__table_display_wants.identify_region(event.x, event.y) == "separator":
+            return "break"  # Prevent dragging if clicked on a separator
+
+        # Handle clearing selection on double click
+        selected_item = self.__table_display_wants.selection()
+        current_focus = self.__table_display_wants.focus()
+
+        if selected_item:
+            if current_focus == selected_item[0]:
+                # Deselect if the same item is clicked again
+                self.__table_display_wants.selection_remove(selected_item[0])
+                self.__table_display_wants.selection_clear()
+                return "break"  # Prevent any further processing
+
+        # Update the last clicked item
+        self.last_clicked_item = current_focus
+
     def GET_RELEVANT_PATHDIR(self, IMAGENAME):
       path = os.path.dirname(os.path.abspath(__file__))
       return os.path.join(path, IMAGENAME)
